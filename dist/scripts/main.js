4018ae7047f05c15ba3e9647b95e15f30c4ac6fd
Kristen Rieger <karieger1@gmail.com>
2015-05-28 18:51:07 -0500

    login done minus jquery

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -12,25 +12,44 @@ $(document).on("ready", function(){
 // 		}
 
 
-	var $button = $("button");
+// 	var $button = $("button");
 
-	$('#button').click(function(){
+// 	$('#button').click(function(){
 
-	    $(this).attr('disabled');
-	 		$button.html("Loading...")
-	 		$button.css("color", "lightgrey");
+// 	    $(this).attr('disabled');
+// 	 		$button.html("Loading...")
+// 	 		$button.css("color", "lightgrey");
 
-	 	setTimeout(button, 4000);
-	 });
+// 	 	setTimeout(button, 4000);
+// 	 });
 
-	 function changeBack(e) {
-		$btn.html('Submit');
-		$btn.css('color', '#black');
+// 	 function changeBack(e) {
+// 		$btn.html('Submit');
+// 		$btn.css('color', '#black');
+// 	}
+// //I can't figure out why it won't revert back. It's giving me an error "unexpected identifier VM145:1"?
+
+//TABS PROBLEM
+
+
+
+	var $targetSection = $(window.location.hash);
+	if(!$targetSection.length) {
+		$targetSection = $("main section").eq(0);
 	}
-//I can't figure out why it won't revert back. It's giving me an error "unexpected identifier VM145:1"?
+	$targetSection.show();
+
+	$('nav a[href="#' +$targetSection.attr('id')+'"]').addClass('active');
+
+	$('nav').on('click', function(e) {
+		var $target = $(e.target);
+		var targetName = $target.attr('href');
+		$('main section').hide();
+		$(targetName).show();
 
 
+		$('nav a').removeClass('active');
+		$target.addClass('active');
 
-
-
+	});
 });
