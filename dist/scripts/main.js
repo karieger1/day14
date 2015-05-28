5913e33c52564bcefccb7adee99629197a33debb
Kristen Rieger <karieger1@gmail.com>
2015-05-28 15:30:07 -0500

    loading button done

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,15 +1,34 @@
 $(document).on("ready", function(){
-	var $button = $("#likeButton");
-		$button.on("click", logLikes);
-		var clicks = 0;
-		function logLikes(e) {
-			$button.html("1 like");
-			clicks++;
-			$button.html(clicks + " likes");
-				if (clicks === 1) {
-					$button.html(clicks + " like");
-				}
-		}
+// 	var $button = $("#likeButton");
+// 		$button.on("click", logLikes);
+// 		var clicks = 0;
+// 		function logLikes(e) {
+// 			$button.html("1 like");
+// 			clicks++;
+// 			$button.html(clicks + " likes");
+// 				if (clicks === 1) {
+// 					$button.html(clicks + " like");
+// 				}
+// 		}
+
+
+	var $button = $("button");
+
+	$('#button').click(function(){
+
+	    $(this).attr('disabled');
+	 		$button.html("Loading...")
+	 		$button.css("color", "lightgrey");
+
+	 	setTimeout(button, 4000);
+	 });
+
+	 function changeBack(e) {
+		$btn.html('Submit');
+		$btn.css('color', '#black');
+	}
+//I can't figure out why it won't revert back. It's giving me an error "unexpected identifier VM145:1"?
+
 
 
 
