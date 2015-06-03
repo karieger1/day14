363904261191533913089001343580f9b41eb6df
Kristen Rieger <karieger1@gmail.com>
2015-06-03 16:13:29 -0500

    loading button done

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,58 +1,16 @@
-$(document).on("ready", function(){
-// 	var $button = $("#likeButton");
-// 		$button.on("click", logLikes);
-// 		var clicks = 0;
-// 		function logLikes(e) {
-// 			$button.html("1 like");
-// 			clicks++;
-// 			$button.html(clicks + " likes");
-// 				if (clicks === 1) {
-// 					$button.html(clicks + " like");
-// 				}
-// 		}
 
+	var $button = $("button");
 
-// 	var $button = $("button");
+	$button.on("click",function(){
 
-// 	$('#button').click(function(){
+	    $(this).attr('disabled');
+	 		$button.html("Loading...")
+	 		$button.css("color", "lightgrey");
 
-// 	    $(this).attr('disabled');
-// 	 		$button.html("Loading...")
-// 	 		$button.css("color", "lightgrey");
-
-// 	 	setTimeout(button, 4000);
-// 	 });
-
-// 	 function changeBack(e) {
-// 		$btn.html('Submit');
-// 		$btn.css('color', '#black');
-// 	}
-// //I can't figure out why it won't revert back. It's giving me an error "unexpected identifier VM145:1"?
-
-//TABS PROBLEM
-
-
-
-	var $targetSection = $(window.location.hash);
-	 if(!$targetSection.length) {
-	 	$targetSection = $("main section").eq(0);
-	 }
-	$targetSection.show();
-
-	$('nav a[href="#' +$targetSection.attr('id')+'"]').addClass('active');
-
-	 $('nav').on('click', function(e) {
-	 	var $target = $(e.target);
-	 	var targetName = $target.attr('href');
-		$('main section').hide();
-	 	$(targetName).show();
-
-
-	 	$('nav a').removeClass('active');
-	 	$target.addClass('active');
-
+	 	setTimeout(button, 4000);
 	 });
 
-
-
-});
+	 function changeBack(e) {
+		$btn.html('Submit');
+		$btn.css('color', '#black');
+}
